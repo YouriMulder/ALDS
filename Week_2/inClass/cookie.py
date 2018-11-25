b2f3b0f7fa13fba8dd69673272b4abc94c544401
Youri <Youri@demuldertjes.nl>
2018-11-25 18:29:58 +0100

    Update

---
+++ Week_2/inClass/cookie.py
@@ -0,0 +1,38 @@
+def cookies(k, A):
+	counter = 0
+	while len(A) > 1 and (min(A) < k):
+		a = min(A)
+		A.pop(A.index(a))
+		b = min(A)
+		A.pop(A.index(b))
+
+
+		newCookie = a + (2 * b)
+		A[A.index(a)] = newCookie
+
+		a = min(A)
+		counter += 1
+
+	if len(A) > 0 and min(A) >= k:
+		return counter
+	else:
+		return -1
+
+
+
+#k = 1000
+#A = list(range(10 ** 6))
+
+#A = [1,2,3,9,10,12,3]
+
+#k = 1000
+#A = [1000, -1, -1, -1 , -1 , -1 ,- 10]
+
+k = 12
+A = [6,7,9,21,3,4,5,7,10,11,13,2,2,2,2,2,2]
+
+
+result = cookies(k, A)
+print(result)
+
+# waarde hoger dan of gelijk aan k
---
+++ Week_2/quickSort.py
@@ -0,0 +1,25 @@
+import random
+
+class quickSort:
+
+
+	def __init__(self, unsortedList):
+		assert len(unsorted) > 0
+		self.quickSortList = unsortedList
+
+	def swap(self, firstIndex, secondIndex):
+		temp = self.quickSortList[secondIndex]
+		self.quickSortList[secondsIndex] = self.quickSortList[firstIndex]
+		self.quickSortList[firstIndex] = temp
+
+
+	def quickSort(self):
+		random.seed()
+		pivotIndex = random.randint(0, len(self.quickSortList))
+		valueOfPivot = unsorted[pivotIndex]
+		self.swapToLastIndex(pivotIndex, len(self.quickSortList) - 1)
+
+		for j 
+
+
+	
